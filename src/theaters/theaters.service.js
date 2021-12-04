2fd4784705af4b11577765928b68e2a5d59785ab
Tom Officer <thomasofficerjr@gmail.com>
2021-12-04 00:46:48 -0800

    theaters controller, theaters service; git status cd ~ cd Desktop

---
+++ src/theaters/theaters.service.js
@@ -0,0 +1,26 @@
+const knex = require("../db/connection");
+const reduceProperties = require("../utils/reduce-properties");
+
+
+//reducer
+const reduceMovies = reduceProperties("theater_id", {
+    title: ["movies", null, "title"],
+    runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+    rating: ["movies", null, "rating"],
+    image_url: ["movies", null, "image_url"],
+});
+
+//list all theaters
+function list() {
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "mt.theater_id", "t.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .select("*")
+    .then(reduceMovies);
+};
+
+
+//exports
+module.exports = {
+  list,
+};
